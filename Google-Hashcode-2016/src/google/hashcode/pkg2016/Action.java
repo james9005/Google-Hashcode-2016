b892811d87bd50843043892555cc2509f5d211c7
James David Todd <james9005@hotmail.co.uk>
2016-02-11 20:20:37 +0000

    added action action commands and paths

---
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Action.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google.hashcode.pkg2016;
+
+/**
+ *
+ * @author james
+ */
+
+
+
+
+public class Action {
+    public Drone d;
+}
---
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/ActionCommands.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google.hashcode.pkg2016;
+
+/**
+ *
+ * @author james
+ */
+public enum ActionCommands {
+
+LOAD, FLY, DELIVER, UNLOAD, WAIT
+    
+}
