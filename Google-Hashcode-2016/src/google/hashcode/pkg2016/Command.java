773d0594bad3746295423a89864d257b074cc799
Luke Fisher <mail@lukefisher.me>
2016-02-11 22:57:35 +0000

    Add more info to a command + to string

--- Google-Hashcode-2016/src/google/hashcode/pkg2016/Command.java
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Command.java
@@ -1,7 +1,18 @@
 package google.hashcode.pkg2016;
 
 public class Command {
-  public Command(String commandName) {
-    // DO NOTHING
+  public String commandName; 
+  public int thingId;
+  public int productTypeId;
+  
+  public Command(String commandName, int thingId, int productTypeId) {
+    this.commandName = commandName;
+    this.thingId = thingId;
+    this.productTypeId = productTypeId;
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("%s %d %d", commandName, thingId, productTypeId);
   }
 }
