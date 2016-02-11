621e81a3eab97f52ec9bdf3e9ebfd1f5177bdc1c
James David Todd <james9005@hotmail.co.uk>
2016-02-11 18:13:48 +0000

    files into netbeans

---
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Drone.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google.hashcode.pkg2016;
+
+/**
+ *
+ * @author james
+ */
+import java.util.List;
+
+public class Drone {
+  public int droneId;
+  
+  public List<Product> inventory;
+}
