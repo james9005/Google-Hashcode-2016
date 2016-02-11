d34dcbe4ced9a811f3d9547730024a8e0a046d2c
Jack Griffiths <jack.griffiths@outlook.com>
2016-02-11 18:48:10 +0000

    warehouse has a stock dictionary

--- Google-Hashcode-2016/src/google/hashcode/pkg2016/Warehouse.java
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Warehouse.java
@@ -1,11 +1,19 @@
 package google.hashcode.pkg2016;
 
+import java.util.HashMap;
+
 public class Warehouse extends GridItem {
 
     public int id;
+    private HashMap<ProductType, Integer> stock;
     
     public Warehouse(int id, int x, int y) {
         super(x, y);
         this.id = id;
+        this.stock = new HashMap<>();
+    }
+    
+    public int getQuantity(ProductType type) {
+        return stock.get(type);
     }
 }
