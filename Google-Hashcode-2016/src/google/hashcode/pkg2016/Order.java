c01dda71a25cf451a414c3d11930069e9099fce9
Luke Fisher <mail@lukefisher.me>
2016-02-11 19:54:14 +0000

    Add addItem method to Order

--- Google-Hashcode-2016/src/google/hashcode/pkg2016/Order.java
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Order.java
@@ -3,9 +3,15 @@ package google.hashcode.pkg2016;
 public class Order extends GridItem {
     
     public int id;
+    public HashMap<ProductType, int> items = new HashMap<ProductType, int>();
     
     public Order(int id, int x, int y) {
         super(x, y);
         this.id = id;
+    }
+    
+    public void addItem(ProductType pt) {
+      int current = items.getOrDefault(pt, 0);
+      items.putIfAbsent(pt, current + 1);
     }
 }
