96a8a1eafd8258696d431920117b1b0bc717c022
Luke Fisher <mail@lukefisher.me>
2016-02-12 23:55:34 +0000

    Take in a quantity for a command

--- Google-Hashcode-2016/src/google/hashcode/pkg2016/Command.java
+++ Google-Hashcode-2016/src/google/hashcode/pkg2016/Command.java
@@ -4,15 +4,17 @@ public class Command {
   public String commandName;
   public int thingId;
   public int productTypeId;
+  public int quantity;
 
-  public Command(String commandName, int thingId, int productTypeId) {
+  public Command(String commandName, int thingId, int productTypeId, int quantity) {
     this.commandName = commandName;
     this.thingId = thingId;
     this.productTypeId = productTypeId;
+    this.quantity = quantity;
   }
 
   @Override
   public String toString() {
-    return String.format("%s %d %d", commandName, thingId, productTypeId);
+    return String.format("%s %d %d %d", commandName, thingId, productTypeId, quantity);
   }
 }
