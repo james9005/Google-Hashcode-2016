50d18620480092ee8b4b1a9e7de5f1c3a9dae422
Jack Griffiths <jack.griffiths@outlook.com>
2016-02-12 20:43:13 +0000

    Added a DeliveryPlan class

---
+++ DeliveryPlan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashCode2016 {
+    public class DeliveryPlan {
+
+        private List<Tuple<int, int, int>> stockPreorders;
+
+        public DeliveryPlan() {
+            stockPreorders = new List<Tuple<int, int, int>>();
+        }
+
+        public int NumberOfTurns { get; set; }
+
+        public IEnumerable<Tuple<int, int, int>> StockPreorders {
+            get { return stockPreorders; }
+        }
+
+        public void PreorderStock(int warehouseId, int productType, int quantity) {
+            stockPreorders.Add(Tuple.Create(warehouseId, productType, quantity));
+        }
+    }
+}
